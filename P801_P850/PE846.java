8e541c618567dbd48c138473580ebc85d477cd2b
Abraham Segers <bramsegers@users.noreply.github.com>
2023-06-04 23:09:08 +0200

    Add files via upload

--- P801_P850/PE846.java
+++ P801_P850/PE846.java
@@ -8,27 +8,30 @@ public class PE846 {
     new PE846().solve(1000000);
   }
 
-  int[] V;
-  int[][] D;
+  int[] NUMS;
+  int[][] PAIR;
 
   void solve(int N) {
 
-    V = new int[200000];
-    D = new int[200000][300];
+    NUMS = new int[200000];
+    PAIR = new int[200000][300];
 
-    int v = V[1] = ++V[0] + 1;
-    for (int p : util.Util.getPrimes(N)){
-      for (int q=1; p>2 && q<=N/p;) V[v++] = q*=p;
-      for (int q=2; p>2 && q<=N/p;) V[v++] = q*=p;
+    int[] PRIMES = new int[N];
+    int v = NUMS[1] = ++NUMS[0] + 1;
+    for (int p=1; (p+=2)<N;){
+      if (PRIMES[p]>0) continue;
+      for (int q=p; (q+=p)<N;) PRIMES[q] = 1;
+      for (int q=1; q<=N/p;) NUMS[v++] = q*=p;
+      for (int q=2; q<=N/p;) NUMS[v++] = q*=p;
     }
 
     for (int i=0; i<v; i++){
       for (int j=i+1; j<v; j++){
-        long r = 1L*V[i]*V[j]-1;
+        long r = 1L*NUMS[i]*NUMS[j]-1;
         long s = (long) Math.sqrt(r);
         if (r>s*s) continue;
-        D[i][++D[i][0]] = j;
-        D[j][++D[j][0]] = i;
+        PAIR[i][++PAIR[i][0]] = j;
+        PAIR[j][++PAIR[j][0]] = i;
       }
     }
 
@@ -37,7 +40,7 @@ public class PE846 {
       .IntStream
       .range(0,i)
       .parallel()
-      .mapToLong(k -> D[k][0]<1 ? 0 : dfs(V[k],k,k,1,new int[i]))
+      .mapToLong(k -> dfs(NUMS[k],k,k,1,new int[i]))
       .sum()/2;
 
     System.out.format("F(%d)=%d%n", N, ans);
@@ -46,13 +49,13 @@ public class PE846 {
   long dfs(long pot, int firsti, int lasti, int len, int[] seen) {
     long sum = 0;
     seen[lasti] = 1;
-    int first = V[firsti];
-    int[] a = D[lasti];
-    for (int i=a[0]; i>0;){
-      int nexti = a[i--];
-      int next = V[nexti];
-      if (first==next && len>2) sum += pot; else
-      if (first< next && seen[nexti]<1) sum += dfs(pot+next, firsti, nexti, len+1, seen);
+    int first = NUMS[firsti];
+    int[] pair = PAIR[lasti];
+    for (int i=pair[0]; i>0;){
+      int nexti = pair[i--];
+      int next = NUMS[nexti];
+      if (first==next && len>2) sum += pot;
+      if (first< next && seen[nexti]<1) sum += dfs(pot + next, firsti, nexti, len + 1, seen);
     }
     seen[lasti] = 0;
     return sum;
