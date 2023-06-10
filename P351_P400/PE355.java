0b9d6b8306f57336d0c32a4ec8122dcf93a20d2f
Abraham Segers <bramsegers@users.noreply.github.com>
2023-06-10 17:53:41 +0200

    Add files via upload

--- P351_P400/PE355.java
+++ P351_P400/PE355.java
@@ -22,11 +22,12 @@ public class PE355{
     int[] a=new int[N], b=new int[N];
     for (t=s,i=0;(p=P[i])*p<=N; i++) if(p>0){
     for (x=p,y=0;x*p<=N;) x*=p; for (q=p;;q*=p){
-    for (j=N/q;Q[j]>0;)j--; if (j<=N/j) break;
-    if (x<q*j-j) {x=q*j-j; y=j;}} if(a[y]>0){
-    P[i]^=-(Q[y]=1); u=f(s+x-p); P[i]^=-1;
-    P[a[y]]^=-1; v=f(s+b[y]); P[a[y]]^=-1;
-    return (Q[y]=0)+(u>v?u:v);} t+=x-p;
-    a[y]=i; b[y]=x-p;} return t;
+    for (j=N/q;Q[j]>0;) j--; if (j<=N/j) break;
+    if (x<q*j-j) x=q*(y=j)-y;} if(a[y]>0){
+      P[i]^=-(Q[y]=1); u=f(s+x-p); P[i]^=-1;
+      P[a[y]]^=-1; v=f(s+b[y]); P[a[y]]^=-1;
+      return (Q[y]=0)+(u>v?u:v);}
+    t+=x-p; a[y]=i; b[y]=x-p;}
+    return t;
   }
 }
